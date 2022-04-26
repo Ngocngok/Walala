25e558f0522d480db8eefa32dd319d395ce7be58
ngocngok <hackerdaongbo@gmail.com>
2022-04-26 23:31:37 +0700

    test physics joint with character controller

---
+++ Assets/DrawOnMeshScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawOnMeshScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject eraserGameObject;
+
+    private Material mainMat;
+
+    private void Start()
+    {
+        mainMat = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        eraserGameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+        if (Input.GetMouseButton(0))
+        {
+            mainMat.SetVector("_EraserPos", Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10));
+        }
+    }
+}
